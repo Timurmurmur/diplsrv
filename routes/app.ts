8e434e4d2c351bf6503992da06b5068226b77bbe
Timur Bartenev <bartimurmurmur@gmail.com>
2019-12-13 19:37:18 +0300

    'added_typescript

---
+++ routes/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { Request, Response } from "express";
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
+  res.send(200);
+});
+
+app.listen(3000);
